f54553ebe87567da000b9c0bd65e96e09af01be2
CodeWithWahid <wahidulislamofficialbd@gmail.com>
2026-01-27 13:52:31 +0600

    for_loop added

--- for_loop.js
+++ for_loop.js
@@ -1,11 +1,26 @@
-let num=0;
-let numbers=[20,32,12,54,23,46,87,34]
+// let num=0;
+// let numbers=[20,32,12,54,23,46,87,34]
 
-for(const num of numbers){
-console.log(num);
+// for(const num of numbers){
+// console.log(num);
+// }
+// for(let i=0;i<=50;i++){
+//  console.log(i);
+// }
+
+// const num1=0;
+// while(num1<5){
+//     console.log('looooooping',num1);
+//     num=num+1;
+// }
+
+let num2=1;
+while(num2 <= 10){
+    console.log(num2);
+    num2++;
 }
-for(let i=0;i<=50;i++){
- console.log(i);
+let num3=1;
+while(num3<=100){
+    console.log(num3);
+    num3++;
 }
-
-
