8de17f54904837938b48aeb6340d5f322fde739a
CodeWithWahid <wahidulislamofficialbd@gmail.com>
2026-01-27 17:29:59 +0600

    commitment file added

---
+++ commitment.js
@@ -0,0 +1,12 @@
+//"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this using for loop.
+
+for(let i=1;i<=60;i++){
+    console.log(i,"I will invest at least 6 hrs every single day for next 60 days!");
+}
+
+let n=1;
+while(n<=60){
+    console.log(n,"I will invest at least 6 hrs every single day for next 60 days!");
+    n++;
+}
+
---
+++ odd-even.js
@@ -0,0 +1,23 @@
+/***
+Subtask-1:
+Find all the odd numbers from 61 to 100.
+ */
+/***
+Subtask-2:
+Find all the even numbers from 78 to 98.
+ */
+/*programming hero*/
+
+
+for(let i=61;i<=100;i++){
+    if(i%2===1){
+        console.log("odd number is:",i);
+    }
+}
+
+for(let i=61;i<=100;i++){
+    if(i%2===0){
+        console.log("even number is:",i);
+    }
+}
+
