8f2e3c70fa5a626a3fde040ab0737110e1baf149
CodeWithWahid <wahidulislamofficialbd@gmail.com>
2026-01-28 10:11:16 +0600

    string slice split join added

--- string.js
+++ string.js
@@ -24,3 +24,19 @@ if(drink ===liquid.trim()){
 else{
     console.log("pani khabo na");
 }
+
+const address="baganbari";
+const part=address.slice(3,6);
+console.log(part);
+const person="I am a good and hardworking person";
+console.log(person.split(``));
+console.log(person.split(` `));
+console.log(person.split(`a`));
+
+const friendsStr="rahim,karim,fahim,sahim,lam";
+const friends=friendsStr.split(',');
+console.log(friends);
+const realFriend =["rahim","karim","fahim","sahim","lam" ];
+console.log(realFriend.join());
+console.log(realFriend.join(`|`));
+console.log(realFriend.join(`-`));
