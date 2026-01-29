5f38954e44cf24e3f6e55369d1e7f839eb3a729a
Mishu78 <meherun2002@gmail.com>
2026-01-29 17:33:37 +0600

    numbers.length added , for of,for and while lop used

--- array-two.js
+++ array-two.js
@@ -2,6 +2,17 @@ const friends=['Elon','Mark','Bill','Waren'];
 for(const friend of friends){
     console.log(friend);
 }
-for(let i=0;i<10;i++){
+for(let i=0;i<friends.length;i++){
     console.log(i);
+    console.log(friends[i]);
+}
+const numbers=[12,233,45,3,4,5,6,213,8,87,56,45];
+for(let i=0;i<numbers.length;i++){
+    console.log(i);
+    console.log("using for loop:", numbers[i]);
+}
+let i=0;
+while(i<numbers.length){
+    console.log("using while loop:", numbers[i]);
+    i++;
 }
