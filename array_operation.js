a4ad3a838729897e99d1317e07005358d8b77b68
Mishu78 <meherun2002@gmail.com>
2026-02-04 13:58:49 +0600

    sum of numbers added

--- array_operation.js
+++ array_operation.js
@@ -1,8 +1,10 @@
 function sumOfNumbers(numbers){
+    let sum=0;
     for(const number of numbers){
 console.log(number);
+sum=sum+number;
     }
-
+    return sum;
 }
 const nums=[54,63,12,6];
 const sum=sumOfNumbers(nums);
