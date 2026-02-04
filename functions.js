3a675a3eee5bb9964528f678001162f5077ff652
Mishu78 <meherun2002@gmail.com>
2026-02-04 13:42:16 +0600

    number of elements added through array

--- functions.js
+++ functions.js
@@ -99,3 +99,37 @@ function isOdd(number){
 
 console.log(isOdd(5));
 console.log(isOdd(100));
+
+/*for a given string tell me whether it has even number of characters or not*/
+function evenSizedString(str){
+    const size=str.length;
+    console.log(str,size);
+    if(str%2===0){
+        console.log("even size");
+        return true;
+    }
+    else{
+        console.log("odd size");
+  return false;
+    }
+      
+}
+evenSizedString('Dhaka');
+evenSizedString('Dhaka city');
+function doubleOrTripple(number,doDouble){
+    if(doDouble ===true){
+        const result=number*2;
+        return result;
+    }
+    else{
+        const result =number*3;
+        return result;
+    }
+}
+console.log(doubleOrTripple(5,true));
+console.log(doubleOrTripple(5,false));
+
+function numberOfElements(numbers){
+    return numbers.length;
+}
+console.log(numberOfElements([12,43,25,78,54,56,76,23]));
