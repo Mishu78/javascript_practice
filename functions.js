6808041519a4cbbefcd21d738197660848a03758
Mishu78 <meherun2002@gmail.com>
2026-02-04 11:21:25 +0600

    argument passes through function

---
+++ functions.js
@@ -0,0 +1,45 @@
+const price=45;
+function fanOffKor(){
+    console.log("bosha theke uthe dara");
+    console.log("walk toward the switch");
+    console.log("click the switch");
+}
+function brushTeeth(){
+ console.log('pick up the brush');
+console.log("add paste");
+}
+
+fanOffKor();
+
+
+function square(number){
+console.log(number);
+const borgo=number*number;
+console.log(borgo);
+}
+square(4);
+function add(num1,num2){
+    const sum=num1+num2;
+    console.log(sum);
+}
+add(4,5);
+
+function doubled (number){
+    const doubled=number*2;
+    console.log(number,doubled);
+}
+doubled(15);
+//console.log(number);
+const num=44;
+doubled(num);
+const number=55;
+doubled(number);
+
+function difference(num1,num2){
+    const diff=num1-num2;
+    console.log(num1,num2,"diff is-",diff);
+}
+difference(43,34);
+const fatherAge=40;
+const myAge=10;
+difference(fatherAge,myAge);
