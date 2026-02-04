f8451f55b9c0c7d72f0f11d97a8c635622d9aa6d
Mishu78 <meherun2002@gmail.com>
2026-02-04 16:54:25 +0600

    issues

--- odd-average-practice.js
+++ odd-average-practice.js
@@ -1,15 +1,22 @@
 /*function takes an array as parameter give me the average of the odd numbers in the array*/ 
 function oddAvrg(numbers){
+    let sum=0;
     const odds=[]
 for(const number of numbers){
     if(number%2===1){
-        odds.push(numbers);
-        console.log("odd numbers",number);
+        odds_array=odds.push(numbers);
+        console.log("odd numbers",odds_array);
     }
     else{
         console.log("even numbers",number);
     }
 }
+for(const odds of numbers){
+    const count=odds.length;
+sum=sum+number;
+const avg=sum/count;
+}
+return avg;
 }
 const odd_num=[12,13,15,11,2,5,56,44,33,35,91,38,7]
 const odd=oddAvrg(odd_num);
