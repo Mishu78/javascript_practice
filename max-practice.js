53ecae9e45a2cd662f01692a9c280896b4688e97
Mishu78 <meherun2002@gmail.com>
2026-02-05 12:06:35 +0600

    maximum number added

---
+++ max-practice.js
@@ -0,0 +1,44 @@
+const disha=56;
+const salman=43;
+if(disha>salman){
+    console.log("disha will get the strawberry")
+}
+
+function getMax(num1,num2){
+  if(num1>num2){
+   return num1;
+  }
+  else{
+    return num2;
+  }
+}
+const max1=getMax(60,40);
+const max2=getMax(50,80);
+const ultimateMax=getMax(max1,max2);
+console.log(ultimateMax);
+
+const jim=56;
+const sim=30;
+const mim=70;
+if(jim>sim && jim>mim){
+    console.log('jim is the ultimate boss');
+}
+else if(sim>jim && sim>mim){
+    console.log('sim is the boss');
+}
+else{
+    console.log('mim is the boss');
+}
+
+function maxOfThree(num1,num2,num3){
+if(jim>sim && jim>mim){
+    console.log('jim is the ultimate boss');
+}
+else if(sim>jim && sim>mim){
+    console.log('sim is the boss');
+}
+else{
+    console.log('mim is the boss');
+}
+}
+const 
