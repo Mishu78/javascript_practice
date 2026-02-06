b5f2c04b7ba1d1eaa9a5f3f4e4b5c079657714b1
programming hero <meherun2002@gmail.com>
2026-02-06 11:44:47 +0600

    product quantity multiplied

--- javascript_practice-main/total-practice.js
+++ javascript_practice-main/total-practice.js
@@ -1,19 +1,19 @@
 const products = [
-  { name: "shirt", price: 3000 },
-  { name: "pant", price: 4500 },
-  { name: "shoe", price: 6000 },
-  { name: "jacket", price: 8000 },
-  { name: "watch", price: 12000 },
-  { name: "cap", price: 800 },
-  { name: "belt", price: 1500 },
-  { name: "bag", price: 5000 },
+  { name: "shirt", price: 3000 ,quantity:2  },
+  { name: "pant", price: 4500  ,quantity:4 },
+  { name: "shoe", price: 6000  ,quantity:5  },
+  { name: "jacket", price: 8000,quantity:1   },
+  { name: "watch", price: 12000,quantity:2   },
+  { name: "cap", price: 800    ,quantity:4},
+  { name: "belt", price: 1500  ,quantity:6  },
+  { name: "bag", price: 5000   ,quantity:3},
 ];
 
 function buyProducts(products){
     let total=0;
     for(const product of products){
         console.log("each products are:",product);
-        total=total+product.price;
+        total=total+product.price*product.quantity;
     }
     return total;
 }
