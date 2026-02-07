f21f4807bb72bb2ade5be6d9321b5829757774f4
programming hero <meherun2002@gmail.com>
2026-02-07 16:24:22 +0600

    problem-3 issue fixed

--- javascript_practice-main/problem-3.js
+++ javascript_practice-main/problem-3.js
@@ -1,9 +1,12 @@
 function finalScore (omr) {
+if(typeof(omr)!=="object" || Array.isArray(omr) || omr===null){
+    return "Invalid";
+}
 if(omr.right+omr.wrong+omr.skip!=100){
     return "Invalid";
 }
 let finalscore=(omr.right*1)-(omr.wrong*0.5);
-score=Math.round(finalscore);
+let score=Math.round(finalscore);
     return score;
 }
 const output=finalScore(["Raj"]);
