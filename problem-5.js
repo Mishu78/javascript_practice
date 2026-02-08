60affc7464c369ce25b05be69fefae3a14aeb95f
Mishu78 <meherun2002@gmail.com>
2026-02-08 14:29:36 +0600

    problem-5 solved

--- problem-5.js
+++ problem-5.js
@@ -1,5 +1,20 @@
 function analyzeText(str) {
- if(typeof str !=="string"){
-    return "Invalid;"
+ if(typeof str !=="string" || str.trim()===""){
+    return "Invalid"
+ }
+ let totalCharacter=str.split(" ").join("").length;
+
+ const words= str.split(" ");
+ let longest=words[0];
+ for(let i=1;i<words.length;i++){
+    if(words[i].length>longest.length){
+        longest=words[i];
+    }
+ }
+ return{
+    longwords:longest,
+    token:totalCharacter,
  }
 }
+const output=analyzeText("hello  world");
+console.log(output);
